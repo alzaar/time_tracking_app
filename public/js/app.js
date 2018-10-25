f48114a84b9f5c69eadcc593b80aee7889c49dc1
Ali Zahir <alizahir@live.com>
2018-10-25 09:09:29 -0400

    almost there

--- public/js/app.js
+++ public/js/app.js
@@ -39,3 +39,71 @@ class EditableTimerList extends React.Component {
     );
   }
 }
+
+class EditableTimer extends React.Component {
+  render() {
+    if (this.props.editFormOpen)
+      return (
+        <TimerForm
+          title={this.props.title}
+          project={this.props.project}
+        />
+      );
+    } else {
+      return (
+        <Timer
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      );
+    }
+  }
+}
+
+class TimerForm extends React.Component {
+  render() {
+    const submitText = this.props.title ? 'Update' : 'Create';
+    return (
+      <div className="ui centered Card">
+        <div className="content">
+          <div className="ui form">
+            <div className="field" >
+              <label>Title</label>
+              <input type="text" defaultValue={this.props.title} />
+              </div>
+              <div className='field'>
+                <label>Project</label>
+                <input type="text" defaultValue={this.props.project} />
+              </div>
+              <div className="two bottom attached buttons">
+                <button className="ui basic blue button">
+                  {submitText}
+                </button>
+                <button className="ui basic red button">
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+class ToggleableTimerForm extends React.Component {
+  render() {
+    if (this.props.isOpen) {
+      return (
+        <TimerForm />
+      );
+    } else {
+      <div className="ui basic content center aligned segment">
+        <button className="ui basic button icon">
+          <i className="plus icon" />
+          </button>
+      </div>
+    }
+  }
+}
