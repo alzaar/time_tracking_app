f09b9371264132c6e2f8ce331ec40b88d9d68c1a
Ali Zahir <alizahir@live.com>
2018-10-25 08:35:57 -0400

    tgb

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
 
+import React from "react";
+
+class TimerDashboard extends React.Component {
+  render() {
+    return (
+      <div className="ui three colomn centered grid">
+        <div className="colomn">
+          <EditableTimerList  />
+          <ToggleableTimerForm
+            isOpen={true}
+            />
+        </div>
+      </div>
+    );
+  }
+}
